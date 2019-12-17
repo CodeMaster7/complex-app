484f3f40602abb19c05669591d92b11278a21ff1
sam <sammybonfanti@gmail.com>
2019-12-17 02:36:53 -0700

    added the user logic for the register method

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const User = require('../models/User')
+
+exports.login = function () {
+
+}
+
+exports.logout = function () {
+
+}
+
+exports.register = function (req, res) {
+    let user = new User(req.body) // create a new object using User() as its blue-print // always capitalize blue-prints // capital User() helps us distinguish from the user object // the this keyword from the User model is ponting to the new obj since its calling/executing the User() constructor
+    user.register()
+
+    if (user.errors.length) {
+        res.send(user.errors)
+    } else {
+        res.send('Congrats, there are no errors.')
+    }
+}
+
+exports.home = function (req, res) {
+    res.render('home-guest')
+}

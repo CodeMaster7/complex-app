09abd7f3c88a9d450ed5c639edbb4cd2f1679ea6
sam <sammybonfanti@gmail.com>
2019-12-20 21:53:17 -0700

    more register logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,19 +23,25 @@ exports.logout = function (req, res) {
 
 exports.register = function (req, res) {
     let user = new User(req.body) // create a new object using User() as its blue-print // always capitalize blue-prints // capital User() helps us distinguish from the user object // the this keyword from the User model is ponting to the new obj since its calling/executing the User() constructor
-    user.register()
-
-    if (user.errors.length) {
-        res.send(user.errors)
-    } else {
-        res.send('Congrats, there are no errors.')
-    }
+    user.register().then(() => {
+        req.session.user = {username: user.data.username}
+        req.session.save(function () {
+            res.redirect('/')
+        })
+    }).catch((regErrors) => {
+        regErrors.forEach(function (error) {
+            req.flash('regErrors', error)
+        })
+        req.session.save(function () {
+            res.redirect('/')
+        })
+    })
 }
 
 exports.home = function (req, res) {
     if (req.session.user) {
         res.render('home-dashboard', {username: req.session.user.username})
     } else {
-        res.render('home-guest', {errors: req.flash('errors')})
+        res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
     }
 }

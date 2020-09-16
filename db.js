c746429ee2ad876a9ce877880ea21ab3353eaeae
sam <sammybonfanti@gmail.com>
2020-09-15 23:33:20 -0600

    new changes

--- db.js
+++ db.js
@@ -6,10 +6,16 @@ const {
     SERVER_PORT
 } = process.env;
 
+//add this change for heroku and change port in the app.listen inside the mongodb.connect
+let port = process.env.PORT
+if (port == null || port == '') {
+    port = SERVER_PORT
+}
+
 // connect to the database
 mongodb.connect(CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true}, function (err, client) {
     console.log('connected to database');
     module.exports = client
     const app = require('./app')
-    app.listen(SERVER_PORT)
+    app.listen(port)
 })
